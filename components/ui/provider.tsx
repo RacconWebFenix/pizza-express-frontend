40bb8f44a4a3b95cf2a533203ab8829aad44eeda
Marcos Domingues <dfenixweb@gmail.com>
2025-06-19 18:35:12 -0300

    feat: refactor Home component layout and integrate Chakra UI; add pizza-themed color palette

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -1,15 +1,13 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+import { ChakraProvider } from "@chakra-ui/react";
+import { pizzaSystem } from "./pizzaTheme";
+import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={pizzaSystem}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
-  )
+  );
 }
