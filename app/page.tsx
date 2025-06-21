3ab2f6ea659b19c73b2bb6af99026dd227de139e
Marcos Domingues <dfenixweb@gmail.com>
2025-06-21 10:31:52 -0300

    Atualizado instructions.txt com informações detalhadas sobre estrutura de pastas, dependências e configuração do ambiente

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import TestChakra from "../components/TestChakra";
+
 
 export default function Home() {
   return (
@@ -90,6 +92,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <TestChakra />
     </div>
   );
 }
---
+++ components/TestChakra.tsx
@@ -0,0 +1,9 @@
+import { Box, Button } from "@chakra-ui/react";
+
+export default function TestChakra() {
+  return (
+    <Box textAlign="center" fontSize="xl" p={5}>
+      <Button colorScheme="teal">Test Chakra UI</Button>
+    </Box>
+  );
+}
