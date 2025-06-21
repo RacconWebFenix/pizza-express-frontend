d08ab52ec9b624cba1f435f13e685dff17855427
Marcos Domingues <dfenixweb@gmail.com>
2025-06-21 08:54:31 -0300

    fix: update test to use act from React and upgrade @testing-library/react to resolve deprecation warnings

--- __tests__/sample.test.js
+++ __tests__/sample.test.js
@@ -1,9 +1,11 @@
-
 const React = require("react");
 const { render, screen } = require("@testing-library/react");
+const { act } = require("react");
 
 test("renders a sample test", () => {
-  render(React.createElement("div", null, "Hello, Jest!"));
+  act(() => {
+    render(React.createElement("div", null, "Hello, Jest!"));
+  });
   const element = screen.getByText("Hello, Jest!");
   expect(element).toBeInTheDocument();
 });
